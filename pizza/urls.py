c80c0485b04e278cb34a689f097ca1330656a976
Askin <asknkskk@gmail.com>
2022-09-03 16:10:47 +0300

    orders added

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,6 +1,7 @@
-from .views import home
+from .views import home, order
 from django.urls import path
 
 urlpatterns = [
     path('', home, name="home"),
+    path('order/', order, name="order"),
 ]
