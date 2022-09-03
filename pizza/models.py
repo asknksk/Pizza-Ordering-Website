e150ee98adefc79d960ac7e1a02f6558857eb96e
Askin <asknkskk@gmail.com>
2022-09-03 14:52:48 +0300

    forms and models

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title  # This is for good visual experimentation!
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=100)
+    topping2 = models.CharField(max_length=100) 
+    size = models.ForeignKey(Size, on_delete=models.CASCADE) # This is for correlation to the Size class
+    
+    def __str__(self):
+        return self.size, self.topping1, self.topping2
